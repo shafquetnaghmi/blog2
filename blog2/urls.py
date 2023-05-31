ec54219ec29254988c73be2e9d670c4cc1604ebd
Shafquet Naghmi <naghmishafquet@gmail.com>
2023-05-31 11:38:16 +0530

    'm'

--- blog2/urls.py
+++ blog2/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("",include('app1.urls'),name="blog"),
+    path("",include('app1.urls')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
