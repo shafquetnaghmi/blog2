8c4f0fba8e7612fc98bf18a8816f8376b4823654
Shafquet Naghmi <naghmishafquet@gmail.com>
2023-05-31 11:25:11 +0530

    'm'

--- blog2/settings.py
+++ blog2/settings.py
@@ -87,7 +87,7 @@ WSGI_APPLICATION = 'blog2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE':'django.db.backends.postgresql_psycopg2',
         'NAME': 'postgres',
         'USER':'postgres',
         'PORT':'5432',
